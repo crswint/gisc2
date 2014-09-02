54c26ca86afb1e1093c435ea29ad375a0d6e8ff5
crswin5726 <crswin5626@ung.edu>
2014-09-02 18:29:23 -0400

    Created a new model and set up REST API
    A new model called "Unit" was created to hold our data.
    A serializer was added in order to render data as json/xml.
    A filter was added to json_view.py in order to sort by id field.
    api_urls.py was added in order to display the units.
    initial_data.json was used in order to practice migration.
    Added "rest_framework"and "django filters"  to settings.py

--- apps/units/models.py
+++ apps/units/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Unit(models.Model):
+    """This model holds GPS units"""
+    name = models.CharField(max_length=40)
+    status = models.BooleanField(default=True)
+    desc = models.CharField(max_length=280)
+
+    def __str__(self):
+        return "{}".format(self.name)
